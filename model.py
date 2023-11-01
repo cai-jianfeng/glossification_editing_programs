0035ec4e7b92f52d192a1929c02ffe6462a78c0f
cai-jianfeng <1622613693@qq.com>
2023-11-01 22:18:27 +0800

    update model code and relative paper

---
+++ model.py
@@ -0,0 +1,46 @@
+# _*_ coding:utf-8 _*_
+"""
+@Software: (已读)glossification
+@FileName: model.py
+@Date: 2023/11/1 19:25
+@Author: caijianfeng
+"""
+import torch
+from torch import nn
+
+"""
+the architecture of the generator and executor
+"""
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class Executor(nn.Module):
+    def __init__(self):
+        super(Executor, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class Transformer_encoder(nn.Module):
+    def __init__(self):
+        super(Transformer_encoder, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+class Transformer_decoder(nn.Module):
+    def __init__(self):
+        super(Transformer_decoder, self).__init__()
+        pass
+
+    def forward(self):
+        pass
