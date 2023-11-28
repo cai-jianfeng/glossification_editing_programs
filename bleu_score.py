c804708392a61e03506dcf41057992771c301517
cai-jianfeng <1622613693@qq.com>
2023-11-28 11:54:19 +0800

    update metrics - bleu score

--- bleu_score.py
+++ bleu_score.py
@@ -101,3 +101,32 @@ def n_gram(sentence, n):
         for _ in range(i):
             next(item, None)
     return zip(*sentence_n)
+
+
+if __name__ == '__main__':
+    from nltk.translate.bleu_score import corpus_bleu
+
+    references = [
+        [
+            ['It', 'is', 'a', 'guide', 'to', 'action', 'that',
+             'ensures', 'that', 'the', 'military', 'will', 'forever',
+             'heed', 'Party', 'commands'],
+            ['It', 'is', 'the', 'guiding', 'principle', 'which',
+             'guarantees', 'the', 'military', 'forces', 'always',
+             'being', 'under', 'the', 'command', 'of', 'the', 'Party'],
+            ['It', 'is', 'the', 'practical', 'guide', 'for', 'the',
+             'army', 'always', 'to', 'heed', 'the', 'directions',
+             'of', 'the', 'party']
+        ]
+    ]
+
+    predict = [
+        ['It', 'is', 'a', 'guide', 'to', 'action', 'which',
+         'ensures', 'that', 'the', 'military', 'always',
+         'obeys', 'the', 'commands', 'of', 'the', 'party']
+    ]
+
+    bleu = bleu_score(predict, references)
+    print(bleu)
+    bleu = corpus_bleu(references, predict)
+    print(bleu)
